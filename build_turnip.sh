1e8efba38c450fb2a8729fe3a96cc343b97c6960
playbyan1453 <62523229+playbyan1453@users.noreply.github.com>
2025-03-30 02:58:40 +0700

    Enable dri option on glx flag

--- build_turnip.sh
+++ build_turnip.sh
@@ -119,7 +119,7 @@ build_mesa() {
 
     meson setup "$BUILD_DIR" --cross-file "$CROSS_FILE" --prefix /usr --libdir "$LIBDIR" \
         -D platforms=x11,wayland -D gallium-drivers=freedreno -D vulkan-drivers=freedreno \
-        -D freedreno-kmds=msm,kgsl -D buildtype=release -D glx=disabled \
+        -D freedreno-kmds=msm,kgsl -D buildtype=release -D glx=dri \
         -D egl=disabled -D gles1=disabled -D gles2=disabled \
         -D gallium-xa=disabled -D opengl=false -D shared-glapi=disabled \
         -D b_lto=true -D b_ndebug=true -D cpp_rtti=false -D gbm=disabled \
